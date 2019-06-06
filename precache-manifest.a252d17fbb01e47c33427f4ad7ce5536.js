7f29182415b97d027d74b0b0ce38c9ed62f68b35
Menion93 <andrea.salvoni93@gmail.com>
2019-06-06 18:25:23 +0200

    upload

---
+++ precache-manifest.a252d17fbb01e47c33427f4ad7ce5536.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d185a34d11de4b8b1a89",
+    "url": "/static/js/main.d185a34d.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "d185a34d11de4b8b1a89",
+    "url": "/static/css/main.6ddd053d.chunk.css"
+  },
+  {
+    "revision": "ccf5a4eda5cf6728442ded5bf8522cd9",
+    "url": "/index.html"
+  }
+];
